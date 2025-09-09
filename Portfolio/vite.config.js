58641ab12c44a51d1f291297ad4046e306eb2016
Beau <77253127+ieBeau@users.noreply.github.com>
2025-09-09 11:39:06 -0400

    Add preview host configuration and update preview script in package.json

--- Portfolio/vite.config.js
+++ Portfolio/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [
+      'https://comp229-portfolio-245p.onrender.com'
+    ]
+  }
 })
