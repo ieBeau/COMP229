3306af9791eb7f58d4ba70adbe23b2ba50c26b4d
Beau <77253127+ieBeau@users.noreply.github.com>
2025-10-07 10:22:14 -0400

    Init commit

--- Assignment 2/config/config.js
+++ Assignment 2/config/config.js
@@ -1,7 +1,7 @@
 const config = {
     env: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 3000,
-    jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
+    jwtSecret: process.env.JWT_SECRET,
     mongoUri: process.env.MONGODB_URI ||
     "mongodb+srv://contactpmoreau_db_user:abcdR9SwsFLRWTZn@comp229.ufgw2qm.mongodb.net/Portfolio?retryWrites=true&w=majority&appName=COMP229" ||
     process.env.MONGO_HOST || 'mongodb://' + (process.env.IP || 'localhost') + ':' + (process.env.MONGO_PORT || '27017') + '/mernproject'
